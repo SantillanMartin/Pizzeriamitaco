879c6243cf8237332f86c0ad09d5335a19abccac
SantillanMartin <martinsantillan.a1@gmail.com>
2023-02-04 18:25:13 -0300

    agregue menus y precios

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 let botonMostrarMenuPizza=document.getElementById('boton-menu-pizza');
 let contenedorDiferentesMenus=document.getElementById('main-contenedor-diferentes-menus');
 botonMostrarMenuPizza.onclick=()=>{
+    
+    let mostrarDisplay=document.getElementById("mostrar-display-block");
+    mostrarDisplay.style.display="flex";
+    /*mostrarMenuPizzaParty();*/
+    window.scroll(0,5000);
+}
+
+
+
+function mostrarMenuPizzaParty(){
     contenedorDiferentesMenus.innerHTML="";
     menuPartyPizza=document.createElement('div');
+    menuPartyPizza.innerHTML="<section><h3>Como en casa</h3><h3>Pizzá Mi Taco</h3><h3>Premiun</h3></section>";
     menuPartyPizza.className="menu-party-pizza";
     contenedorDiferentesMenus.append(menuPartyPizza);
+    window.scroll(0,5000);
 }
